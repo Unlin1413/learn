e33073ffec09a57b8d8dbd092a26be42227a9a6e
IDIvanov <74491732+acce1erat0r@users.noreply.github.com>
2024-10-03 22:48:53 +0300

    залупа

--- learn_danya/museum/Artifact2.java
+++ learn_danya/museum/Artifact2.java
@@ -1,10 +1,11 @@
 package museum;
 
 public class Artifact2 extends Relikviya {
-    public Artifact2(int number, String culture){
+    public Artifact2(int number, String culture) {
         super(number, culture, null);
     }
-    public void artifactPerv(){
+
+    public void artifactPerv() {
         System.out.println("Номер этого артифакта: " + super.number + " Культура этого артифакта: " + this.culture);
     }
 }
